aca883b78e74b9f00b77abbe35507244d4feb4c3
Beyonce Knowles <you@example.com>
2021-02-28 21:12:31 -0500

    logging info

--- src/Cats/cats.js
+++ src/Cats/cats.js
@@ -2,40 +2,30 @@ import React from 'react';
 //import config from '../config';
 import Navigation from '../Navigation/navigation';
 import ApiContext from '../ApiContext';
+//import queueInformation from '../queueInformation';
 
 export default class Cats extends React.Component {
+
+   
   static contextType = ApiContext;
+
   
   render() {
-    const allCats = this.context.cats;
-    const jsonString = JSON.stringify(allCats)
-    console.log(allCats)
-    //const parsed = JSON.parse(allCats)
-    const stringed = JSON.stringify(allCats)
-
-    console.log(stringed['breed'])
-    const cats = Object.entries(allCats);
-    console.log(cats)
-    const theArray = []
-    theArray.push(cats)
-    console.log(theArray)
-    const result = Object.keys(allCats).map(e=>allCats[e]);
-
-    console.log(result)
-
-  
+    const firstCat = this.context.cats;
+ 
     return (
       <div className='cats'>
         <Navigation />
         <h1>Meow</h1>
-        <p>Here are all our cats!</p>
-        <ul>
-          <li>Next cat to Adopt<br />
-          {jsonString}
-          {stringed}
+        <p>Next cat to Adopt<br />
+        Name: {firstCat['name']}<br />
+        Gender: {firstCat['gender']}<br />
+        Age: {firstCat['age']}<br />
+        Description: {firstCat['description']}<br />
+        Breed: {firstCat['breed']}<br />
+
           
-        </li>        
-        </ul>
+      </p>
       </div>
     )
   }
