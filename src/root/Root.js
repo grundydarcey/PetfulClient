d1eb716d720e2133b843c0294fe56f9465fc0216
Beyonce Knowles <you@example.com>
2021-03-06 18:37:49 -0500

    mobile

--- src/root/Root.js
+++ src/root/Root.js
@@ -11,10 +11,10 @@ function Root() {
     <div className='root'>
       <Navigation />
       <h2>Are you considering pet adoption? It's a big responsibility with so many purrks. Here at the Petful animal shelter, we take the health and safety of our animals very seriously. If you would like to move forward with the adoption process you can begin the 'Start Adoption' form below and see what it takes to become the owner of one of our amazing pets.</h2>
-      <p>You will have to provide your name so that we may review your eligibility for adopting one of our pets.</p>
+      <p>You will have to provide your name so that we may review your eligibility for adopting one of our pets.</p><br />
       <div className='linksimages'>
-        <Link to='/adoptionprocess'>Start Adoption</Link><br /><br />
-        <p>Feel free to visit some of the navigation links up top to see the animals currently up for adoption!</p>
+        <Link to='/adoptionprocess'>Start Adoption</Link><br /><br /><br />
+        
         <img src={Paperwork} alt='paperwork' className='paperwork' />
         <div className='availablepets'>
           <PetDisplay />
