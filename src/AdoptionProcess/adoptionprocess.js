050d7d987fa4d5473ccb5f783e5623ae28736033
Beyonce Knowles <you@example.com>
2021-03-07 00:02:35 -0500

    rs

--- src/AdoptionProcess/adoptionprocess.js
+++ src/AdoptionProcess/adoptionprocess.js
@@ -327,7 +327,7 @@ export default class AdoptionProcess extends React.Component {
       <p>Thanks for submitting your name! You will be added to our list.</p>
     )
     
-    const determineYourTurn = (this.context.people.first['value'] === this.context.addedUser['value']) ? (
+    const determineYourTurn = (this.context.people.first['value'] === this.context.addedUser) ? (
       <div className='adoption'>
         <Navigation />
         <h2>Choose Your Pet</h2>
